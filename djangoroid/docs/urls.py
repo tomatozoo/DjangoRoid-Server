0037f467a707fd9b20de2251ddef98b70a93aea7
tomatozoo <enkeejuniour@snu.ac.kr>
2023-02-22 18:12:08 +0900

    feat: Implement drf-yasg documentation

--- djangoroid/docs/urls.py
+++ djangoroid/docs/urls.py
@@ -1,7 +1,26 @@
 from django import urls
+from django.conf import settings
 from rest_framework import permissions
 
-from docs import views as doc_views
+from drf_yasg import openapi
 from drf_yasg import views as drf_views
 
-urlpatterns = []
+schema_view = drf_views.get_schema_view(
+    openapi.Info(
+        title="DjangoRoid API",
+        default_version="v1",
+        description="DjangoRoid API 문서입니다",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="enkeejuniour@snu.ac.kr"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+if settings.DEBUG:
+    urlpatterns = [
+        urls.re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        urls.re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+        urls.re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    ]
