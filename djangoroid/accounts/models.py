5cda3e912535d60a25b0bad8c7967620ad5fdfa6
Jaeyeong <rlawodud112294@gmail.com>
2023-02-23 21:43:50 +0900

    tag added

--- djangoroid/accounts/models.py
+++ djangoroid/accounts/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tag.models import Tag
 
 
 class CustomUser(User):
-    nickname = models.CharField(max_length=50, default="Anonymous")   
+    nickname = models.CharField(max_length=50, blank=False)   
+
+
+class UserToTag(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tag')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    
+    
