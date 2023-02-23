0a1273e39e64e140ea210afc76b2c5e56f24371c
Jaeyeong <rlawodud112294@gmail.com>
2023-02-23 19:20:26 +0900

    logout added

--- djangoroid/accounts/urls.py
+++ djangoroid/accounts/urls.py
@@ -1,9 +1,16 @@
 from django import urls
+from django.contrib import auth
+from django.contrib.auth import views as auth_views
 
 from accounts import views
 
 urlpatterns = [
     urls.path("signup/", views.signup),
     urls.path("login/", views.login),
-    # urls.path("accounts/logout/", ),
+    urls.path("logout/", views.logout),
 ]
+
+# urlpatterns = [
+#     urls.path('login/', auth_views.LoginView.as_view(), name='login'),
+#     urls.path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+# ]
