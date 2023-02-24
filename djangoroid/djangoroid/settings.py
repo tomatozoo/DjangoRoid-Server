9abc3795c25e6d8b7a208180db4b5853810bddd8
tomatozoo <enkeejuniour@snu.ac.kr>
2023-02-24 16:24:45 +0900

    feat: Implement profile api

--- djangoroid/djangoroid/settings.py
+++ djangoroid/djangoroid/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-7ful-9r9hf0mm-%=@^^a9la=(&uyq15_ucj%551x0-svp-l%^g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,6 +45,12 @@ INSTALLED_APPS = [
     "tag.apps.TagConfig",
     "rest_framework.authtoken",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
