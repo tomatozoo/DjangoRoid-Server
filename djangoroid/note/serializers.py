4dfa5ddb9213df7b3d87422b76b1b9d9d94c8444
tomatozoo <enkeejuniour@snu.ac.kr>
2023-02-25 00:44:55 +0900

    feat: Add image update/create

--- djangoroid/note/serializers.py
+++ djangoroid/note/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 
-from note.models import Note, NoteToTag
+from note.models import Note, NoteToTag, Canvas, Page
 from tag.models import Tag
+
 
 def get_tag_note_objects(note):
     tags = NoteToTag.objects.filter(note=note)
     return tags
 
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page
+        fields = "__all__"
+
+
+class CanvasSerializer(serializers.ModelSerializer):
+    pages = PageSerializer(many=True, read_only=True, required=False)
+
+    class Meta:
+        model = Canvas
+        fields = ("id", "created_at", "updated_at", "pages")
 
 
 class NoteListSerializer(serializers.ModelSerializer):
@@ -15,19 +29,20 @@ class NoteListSerializer(serializers.ModelSerializer):
         tags = get_tag_note_objects(instance)
         ret['tags'] = [tag.tag.name for tag in tags]
         return ret
+
     class Meta:
         model = Note
         fields = ['id', 'title', 'description', 'waffle_count', 'fork_count',
                   'is_public', 'created_by', 'created_at', 'updated_at']
 
 
-class NoteDetailSerializer(serializers.ModelSerializer): 
+class NoteDetailSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         tags = get_tag_note_objects(instance)
         ret['tags'] = [tag.tag.name for tag in tags]
         return ret
-               
+
     class Meta:
         model = Note
         fields = '__all__'
