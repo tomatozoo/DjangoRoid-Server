10664dc5ffeaa18a20b03597b5ca185467aeb4e2
Jaeyeong <rlawodud112294@gmail.com>
2023-02-25 03:48:42 +0900

    permission fixed

--- djangoroid/note/views.py
+++ djangoroid/note/views.py
@@ -103,20 +103,54 @@ class NoteCreateView(generics.CreateAPIView):
     serializer_class = NoteDetailSerializer
     authentication_classes = [authentication.TokenAuthentication]
 
-
+# 만약에 note가 private이면, 유저만 수정할 수 있음
+#         만약 note가 public이면 로그인한 유저는 누구나 수정할 수 있음
+#         만약에 delete method이면 생성한 유저만 삭제할 수 있음
+#         contributor도 지정하기
 class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         user_id = self.kwargs['userPk']
         note_id = self.kwargs['notePk']
         note = get_object_or_404(Note, created_by=user_id, id=note_id)
 
-        if not self.request.method in permissions.SAFE_METHODS:
+        if self.request.method in permissions.SAFE_METHODS:
+            return note
+        
+        if self.request.method == 'DELETE':
+            self.check_object_permissions(self.request, note)
+            return note
+        
+        if self.request.method in ['PATCH', 'PUT']:        
+            if note.is_public:
+                user = get_user(self.request)
+                if not isinstance(user, User):
+                    return "No permission"
+            else:
+                self.check_object_permissions(self.request, note)
+        if self.request.method == 'delete':
             self.check_object_permissions(self.request, note)
         return note
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        if instance == "No permission":
+            return Response(data={'detail' : "No permission(put, patch)"}, 
+                            status=status.HTTP_401_UNAUTHORIZED)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
     serializer_class = NoteDetailSerializer
     permission_classes = [IsNoteCreator]
-    authentication_classes = [authentication.TokenAuthentication]
+    # authentication_classes = [authentication.TokenAuthentication]
 
 
 def get_user(request):
